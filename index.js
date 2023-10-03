13d6b12c1f67f481d1d1a682bc94175b20c4c349
Hisham Al-Eesa <hisham.aleesa@gmail.com>
2023-10-03 13:48:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+    let cardsArray = [];
+    for(let i = 0 ; i < names.length; i++){
+        cardsArray[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return cardsArray;
+}
+
+function countDown(posInteger){
+    while(posInteger >= 0){
+        console.log(posInteger);
+        posInteger--;
+    }
+}
